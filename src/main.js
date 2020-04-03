6195f32f2553e650232368e0f7e4500c82660bb2
trueroma <48531151+trueroma@users.noreply.github.com>
2020-04-03 10:49:46 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -72,7 +72,7 @@ const decorateNumber = n => {
   const digits = Array.from((+n).toString());
   const view = [];
 
-  for (let i = 1; digits.length; i++) view.unshift(i % 4 ? digits.pop() : '`');
+  for (let i = 1; digits.length; i++) view.unshift(i % 4 ? digits.pop() : ' ');
 
   return view.join('');
 };
